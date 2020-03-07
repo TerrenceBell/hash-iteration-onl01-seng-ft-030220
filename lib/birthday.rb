d30fa84bfbf9798317faa58fed7c058f9792820b
Terrence Bell <terrencekbell@gmail.com>
2020-03-07 03:17:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,8 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do 
-    |kids_name, age|
-    if age < 13
+    |kids_name, age| if age < 13
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   else 
     puts "You are too old for this"
